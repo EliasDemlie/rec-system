01a56a8ff4215a4dee1bc204fce878ca5b7887be
eliasdemlie <elias.dm257@gmail.com>
2025-05-27 01:55:49 +0300

    fix : fix reference error

--- config.py
+++ config.py
@@ -4,9 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-SECRET_KEY = os.getenv("SECRET_KEY")
-MODEL_PATH = os.getenv("MODEL_PATH")
+# SECRET_KEY = os.getenv("SECRET_KEY")
 
 PRODUCT_DB_NAME = os.getenv("PRODUCT_DB_NAME")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
